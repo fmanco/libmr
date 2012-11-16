4f0864e30505fe604efe97fddb7d72f9c85b743b
Filipe Manco <filipe.manco@ua.pt>
2012-11-16 23:17:59 +0000

    Add Makefile and dummy app.c and base.h

---
+++ Makefile
@@ -0,0 +1,36 @@
+# ===========================================================================
+# mrlib - A lowlevel library for "Micro Rato"
+# ===========================================================================
+
+##
+ #  \file Makefile
+ #
+ #  \brief Basic Makefile for a mrlib based application
+ #
+ #  This Makefile builds an application name app.c placed on app folder.
+ #  It also allows to upload the application to the MCU.
+ #
+ #  \author Filipe Manco <filipe.manco@gmail.com>
+ ##
+
+
+APP = app/app.c
+INC = inc/
+LIB = $(shell find lib/ -name *.c -printf "%p ")
+
+
+all:
+	CPATH=$(INC) pcompile $(APP) $(LIB)
+
+doc:
+	doxygen doc/Doxyfile
+
+upload:
+	ldpic32 -w $(patsubst %.c, %.hex, $(APP))
+
+clean:
+	find . -regex ".*\(\.\(hex\|o\|elf\|map\)\|\(~\)\)" -delete -printf "deleted: %p\n"
+	rm -r doc/latex doc/html
+
+
+.PHONY: all doc upload clean
---
+++ app/app.c
@@ -0,0 +1,72 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  app/app.c
+ *  \brief An example application for mrlib
+ *
+ *  This can be used as a base template for a mrlib based application.
+ *
+ *  \version 0.1.0
+ *  \date    Nov 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#include <base.h>
+
+
+/* ========================================================================== */
+
+int main ( void )
+{
+	// mlogInfo(&log, "Jerry is born!");
+
+	/* ======================================================================
+	 * Setup
+	 */
+	// mlogInfo(&log, "Initializing machinery ...");
+
+	// \todo: Initialization code goes here
+
+	// mlogInfo(&log, "Every systems up!");
+
+
+	/* ======================================================================
+	 * Main loop
+	 */
+	// mlogInfo(&log, "Press start to begin the search!");
+
+	// util_waitStart();
+
+	// mlogInfo(&log, "Let the search begin!");
+
+	// while (!state_isFinished()) {
+		// util_waitStep10ms();       // Cycle time is 10ms
+
+		// sensors_update();          // Read sensors to get the results from
+		                           //  the last cycle actuation
+
+
+		// \todo Application code goes here
+
+
+		// lowlevel_implement();      // Implement low level behaviours
+
+		// actuators_update();        // Apply changes to actuators
+	// }
+
+	// mlogInfo(&log, "I got the there!");
+
+	// mlogInfo(&log, "Shuting down systems...");
+
+	// \todo: Stop code goes here
+
+	// mlogInfo(&log, "Jerry is terminated!");
+
+	return 0;
+}
+
+
+/* = EOF ==================================================================== */
