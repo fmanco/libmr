d22d32dec9a4269d9074aa73023b59030a68d85b
Filipe Manco <filipe.manco@ua.pt>
2012-11-17 12:48:42 +0000

    Define actuators interface

---
+++ inc/robot/actuators.h
@@ -0,0 +1,56 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  inc/robot/actuators.h
+ *  \brief Abstraction layer for robot actuation. Allows using of standard
+ *         units, like degrees for servo rotation or m/s for wheels speed.
+ *
+ *
+ *  \version 0.1.0
+ *  \date    Nov 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#ifndef __ROBOT_ACTUATORS_H__
+#define __ROBOT_ACTUATORS_H__
+
+
+#include <base.h>
+
+
+/* ==========================================================================
+ * Management
+ */
+void actuators_init    ( void );
+void actuators_update  ( void );
+void actuators_stop    ( void );
+
+
+/* ==========================================================================
+ * Motors
+ */
+void actuators_setSP   ( int left, int right );
+void actuators_setSPMS ( int left, int right );
+void actuators_getSP   ( int* left, int* right );
+void actuators_getSPMS ( int* left, int* right );
+
+
+/* ==========================================================================
+ * Beacon sensor's servo
+ */
+void actuators_setBeaconSens    ( int degree );
+void actuators_rotateBeaconSens ( int degree );
+
+
+/* ==========================================================================
+ * Leds
+ */
+bool actuators_setLed  ( uint ledN, bool state );
+bool actuators_setLeds ( uint bitmap );
+
+
+/* ========================================================================== */
+#endif /* __ROBOT_ACTUATORS_H__ */
