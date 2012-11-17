6354c9f5f8f70c5c1743ae568312f84eb6326c70
Filipe Manco <filipe.manco@ua.pt>
2012-11-17 10:54:56 +0000

    Define sensors interface
    This is a low level interface for direct interaction with the
    robot. This module will provide filtered values and virtual sensors.

---
+++ inc/robot/sensors.h
@@ -0,0 +1,82 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  inc/robot/sensors.h
+ *  \brief Read robot sensors' values
+ *
+ *  The values provided by this library are already filtered and may
+ *  even not be based on real sensors but derived from other sensors' values.
+ *
+ *  \version 0.1.0
+ *  \date    Nov 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#ifndef __ROBOT_SENSORS_H__
+#define __ROBOT_SENSORS_H__
+
+
+#include <base.h>
+
+
+/* ==========================================================================
+ * Management
+ */
+void sensors_init     ( void );
+void sensors_update   ( void );
+void sensors_stop     ( void );
+
+
+/* ==========================================================================
+ * Obstacles detection
+ */
+int  sensors_obstC     ( void );
+int  sensors_obstL     ( void );
+int  sensors_obstR     ( void );
+
+
+/* ==========================================================================
+ * Beacon search
+ */
+bool sensors_beacon    ( void );
+bool sensors_beaconDir ( void );
+
+
+/* ==========================================================================
+ * Ground detection
+ */
+bool sensors_ground    ( void );
+bool sensors_groundL   ( void );
+bool sensors_groundR   ( void );
+
+
+/* ==========================================================================
+ * Encoders
+ */
+void sensors_encoders  ( int* encL, int* encR );
+
+
+/* ==========================================================================
+ * Compass (simulated)
+ */
+int  sensors_compass   ( void );
+
+
+/* ==========================================================================
+ * Battery level
+ */
+uint sensors_battery   ( void );
+
+
+/* ==========================================================================
+ * Control buttons
+ */
+bool sensors_startBtn ( void );
+bool sensors_stopBtn  ( void );
+
+
+/* ========================================================================== */
+#endif /* __ROBOT_SENSORS_H__ */
