37f173dd9a4d3b1134b81ff7277f3ec6661090a2
Filipe Manco <filipe.manco@ua.pt>
2012-11-19 11:50:32 +0000

    Implement ground sensors related functions

--- lib/robot/sensors.c
+++ lib/robot/sensors.c
@@ -16,26 +16,82 @@
 #include <robot/sensors.h>
 
 
+/* ========================================================================== */
+
+/* ===================
+ * Ground sensors
+ */
+static uint groundCount[5] = {0, 0, 0, 0, 0};
+static bool groundOn[5]    = {false, false, false, false, false};
+
+
+/* ========================================================================== */
+
+static void handleGroundSensor  ( uint value, int sensNr );
+static void updateGroundSensors ( void );
+
+
 /* ==========================================================================
  * Management
  */
 
 void sensors_init ( void )
 {
+	int i;
+
 	robot_enableObstSens();
 	robot_enableGroundSens();
+
+	for (i = 0; i < 5; i++) {
+		groundCount[i] = 0;
+		groundOn[i] = false;
+	}
 }
 
 void sensors_update ( void )
 {
 	robot_readSensors();
 	robot_readEncoders();
+
+	updateGroundSensors();
 }
 
 void sensors_stop ( void )
 {
 	robot_disableObstSens();
 	robot_disableGroundSens();
+}
+
+
+/* ========================================================================== */
+
+static inline void handleGroundSensor ( uint value, int sensNr )
+{
+	if (value) {
+		if (groundCount[sensNr] < GROUND_ST_THRESHOLD)
+			groundCount[sensNr]++;
+	} else {
+		if (groundCount[sensNr] > 0)
+			groundCount[sensNr]--;
+	}
+
+	if (groundOn[sensNr]) {
+		if (groundCount[sensNr] == 0)
+			groundOn[sensNr] = false;
+	} else {
+		if (groundCount[sensNr] == GROUND_ST_THRESHOLD)
+			groundOn[sensNr] = true;
+	}
+}
+
+static void updateGroundSensors ( void )
+{
+	int i;
+	uint sens = sensors.ground;
+
+	for (i = 0; i < 5; i++) {
+		handleGroundSensor((sens & (1 << i)), i);
+	}
 }
 
 
