d5a7e123db6ca2030e8491ed1b0fe4dc57a108aa
Filipe Manco <filipe.manco@ua.pt>
2012-11-19 11:52:04 +0000

    Implement sensors' battery related functions

--- lib/robot/sensors.c
+++ lib/robot/sensors.c
@@ -24,11 +24,17 @@
 static uint groundCount[5] = {0, 0, 0, 0, 0};
 static bool groundOn[5]    = {false, false, false, false, false};
 
+/* ===================
+ * Battery
+ */
+static int battery = 0;
+
 
 /* ========================================================================== */
 
 static void handleGroundSensor  ( uint value, int sensNr );
 static void updateGroundSensors ( void );
+static void updateBattery       ( void );
 
 
 /* ==========================================================================
@@ -46,6 +52,8 @@ void sensors_init ( void )
 		groundCount[i] = 0;
 		groundOn[i] = false;
 	}
+
+	battery = 0;
 }
 
 void sensors_update ( void )
@@ -54,6 +62,7 @@ void sensors_update ( void )
 	robot_readEncoders();
 
 	updateGroundSensors();
+	updateBattery();
 }
 
 void sensors_stop ( void )
@@ -92,6 +101,34 @@ static void updateGroundSensors ( void )
 	for (i = 0; i < 5; i++) {
 		handleGroundSensor((sens & (1 << i)), i);
 	}
+}
+
+/* ===================
+ * Battery update
+ *  - Read battery voltage (average of the last 32 readings)
+ *  - Value is multiplied by 10 (max. value is 101, i.e. 10,1 V)
+ */
+static void updateBattery ( void )
+{
+	static int array[32] = {96, 96, 96, 96, 96, 96, 96, 96,
+	                        96, 96, 96, 96, 96, 96, 96, 96,
+	                        96, 96, 96, 96, 96, 96, 96, 96,
+	                        96, 96, 96, 96, 96, 96, 96, 96};
+	static int i;
+	static int sum = 3072; /* = (96 * 32) */
+
+	uint value;
+
+	value = sensors.battery;
+
+	value = (value * 330 + 511) / 1023;
+	value = (value * (3300 + 6800) + 1650) / 33000;
+
+	sum = sum - array[i] + value;
+	array[i] = value;
+	i = (i + 1) & 0x1F;
+
+	battery = (sum >> 5);
 }
 
 
