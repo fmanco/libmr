25492669f88693d5e7492855f4f2ccdd60099565
Filipe Manco <filipe.manco@ua.pt>
2012-11-19 22:34:59 +0000

    Implement test for sensors module

---
+++ tests/test_sensors.c
@@ -0,0 +1,51 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  tests/test_sensors.c
+ *  \brief Tests for sensors module.
+ *
+ *
+ *  \version 0.1.0
+ *  \date    Nov 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#include <base.h>
+#include <robot/robot.h>
+#include <robot/sensors.h>
+
+
+/* ========================================================================== */
+int main ( void )
+{
+	printStr("Test Sensors started!");
+
+	robot_init();
+	sensors_init();
+
+	while (1) {
+		waitStep10ms();
+
+		sensors_update();
+
+		int encL = 0, encR = 0;
+		int odoL = 0, odoR = 0;
+
+		sensors_encoders(&encL, &encR);
+		sensors_odo(&odoL, &odoR);
+
+		printf("%3d %3d %3d | %d | %d%d%d%d%d | %3d %3d %5d %5d | %3d | %d%d\n",
+			sensors_obstL(), sensors_obstF(), sensors_obstR(),
+			sensors_beacon(),
+			sensors_groundL(), sensors_groundCL(), sensors_groundCF(), sensors_groundCR(), sensors_groundR(),
+			encL, encR, odoL, odoR,
+			sensors_battery(),
+			sensors_startBtn(), sensors_stopBtn());
+	}
+}
+
+
+/* = EOF ==================================================================== */
