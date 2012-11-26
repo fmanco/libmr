891c6e47c8d7b757aef458b97fe0022755ba72f8
Filipe Manco <filipe.manco@ua.pt>
2012-11-26 02:37:34 +0000

    Lower beacon's Schimtt Trigger threshold for faster beacon detection

--- inc/robot/sensors.h
+++ inc/robot/sensors.h
@@ -27,7 +27,7 @@
  */
 
 #define GROUND_ST_THRESHOLD 5
-#define BEACON_ST_THRESHOLD 10
+#define BEACON_ST_THRESHOLD 5
 
 
 /* ==========================================================================
