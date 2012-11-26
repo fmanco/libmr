df89eb577f1f3c6c4c97d54d9fd8a30bd7adf380
Filipe Manco <filipe.manco@ua.pt>
2012-11-26 03:12:44 +0000

    Update inc/robot/conf.h documentation

--- inc/robot/conf.h
+++ inc/robot/conf.h
@@ -6,8 +6,16 @@
  *  \file  inc/robot/conf.h
  *  \brief This header provides robot dependent configurations.
  *
+ *  This header file only provides values that are specific to each physical
+ *  robot, and that cannot be assumed to be the same for all robots, or
+ *  some constant used by the libraries.
+ *
+ *  For instance the maximum and minimum values that can be applied on
+ *  the servo using the robot library are defined in the robot.h header
+ *  file, but the calibration values specific to each servo are defined here.
+ *
  *  The values provided in this header file MUST be calibrated for
-`*  each robot used. Some utilities are provided for the calibration
+ *  each robot used. Some utilities are provided for the calibration
  *  of some values.
  *
  *  \version 0.1.0
