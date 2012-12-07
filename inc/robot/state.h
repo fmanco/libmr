506d3e403fd702158b62a92b98e2f73600b33798
Filipe Manco <filipe.manco@ua.pt>
2012-12-07 02:48:13 +0000

    Implement robot/state module
    This module will be used for information exchange
    in the middleware, mainly between sensors and actuators.

---
+++ inc/robot/state.h
@@ -0,0 +1,45 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  inc/robot/state.h
+ *  \brief This module allows for information exchange between
+ *         the middleware, usually, between sensors and actuators modules.
+ *
+ *
+ *  \version 0.1.0
+ *  \date    Dec 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#ifndef __ROBOT_STATE_H__
+#define __ROBOT_STATE_H__
+
+
+#include <base.h>
+
+
+/* ========================================================================== */
+
+/**
+ * \brief Define the last effective servo degree.
+ *
+ * This is not the raw degree applied by the user but the degree
+ *  with the necessary roundings.
+ *
+ * \param degree The efective servo degree.
+ */
+inline void state_setServoDegree ( int degree );
+
+/**
+ * \brief Get the last announced servo degree.
+ *
+ * \returns The servo degreee.
+ */
+inline int  state_getServoDegree ( void );
+
+
+/* ========================================================================== */
+#endif /* __ROBOT_STATE_H__ */
---
+++ lib/robot/state.c
@@ -0,0 +1,41 @@
+/* ==========================================================================
+ * mrlib - A lowlevel library for "Micro Rato"
+ * ========================================================================== */
+
+/**
+ *  \file  lib/robot/state.c
+ *  \brief Implement the state exchange area.
+ *
+ *  This module is basically a set of shared
+ *   variables with setters and getters.
+ *
+ *
+ *  \version 0.1.0
+ *  \date    Dec 2012
+ *
+ *  \author Filipe Manco <filipe.manco@gmail.com>
+ */
+
+#include <base.h>
+#include <robot/state.h>
+
+
+/* ========================================================================== */
+
+static int servoDegree = 0;
+
+
+/* ========================================================================== */
+
+inline void state_setServoDegree ( int degree )
+{
+	servoDegree = degree;
+}
+
+inline int state_getServoDegree ( void )
+{
+	return servoDegree;
+}
+
+
+/* = EOF ==================================================================== */
