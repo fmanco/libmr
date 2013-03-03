86cde4401ce6395fe52e2d82885acc3ba5aa9a4e
Filipe Manco <filipe.manco@ua.pt>
2013-03-03 00:15:23 +0000

    Improve and document actuators interface
    Implementation for Micro Rato is not in sync.

--- inc/mouse/actuators.h
+++ inc/mouse/actuators.h
@@ -4,12 +4,26 @@
 
 /**
  *  \file  inc/mouse/actuators.h
- *  \brief Abstraction layer for robot actuation. Allows using of standard
- *         units, like degrees for servo rotation or m/s for wheels speed.
+ *  \brief Robot actuation module.
  *
+ *  The actuators module is the module responsible to provide an abstraction
+ *   to the robot actuators.
  *
- *  \version 0.2.0
- *  \date    Dec 2012
+ *  This module assumes a robot with the following characteristics:
+ *   - Differential steering (two similar, parallel and independent wheels);
+ *   - A beacon sensor hooked up to a rotary platform;
+ *   - A set of LEDs available for the user.
+ *
+ *  With the provided functions you can specify low level actuation
+ *   using standard units such as cm/s or degrees.
+ *
+ *  The changes are only applied when actuators_update() is called, and not
+ *   immediately after the call to the function. The changes actually applied
+ *   to the robot are the last ones requested by a call to the correspondent
+ *   function of the module.
+ *
+ *  \version 0.4.0
+ *  \date    Feb 2013
  *
  *  \author Filipe Manco <filipe.manco@gmail.com>
  */
@@ -28,28 +42,34 @@
 /**
  *  \brief Initialize actuators module.
  *
- *  This function resets the actuators module in order it can be used.
- *   It should be called before start using the module
- *   or if you want to reset it.
+ *  This function resets the actuators module in order it can be used, and
+ *   enable it. This function should be called before start using the module
+ *   or whenever you want to reset it.
  *
- *  The changes performed to the state of the robot are immediately
- *   applied, without calling actuators_update().
+ *  *The changes are applied immediately*, without calling actuators_update().
  */
 void actuators_init    ( void );
 
 /**
  *  \brief Apply the requested changes to the actuators.
  *
- *  The changes actually applied to the robot are the last ones requested.
- *  This is, if actuators_setVel() is called twice only the last call will take effect.
+ *  The changes actually applied to the robot are the last ones
+ *   requested by a call to a function.
  */
 void actuators_update  ( void );
 
 /**
  *  \brief Perform the necessary changes in order to stop the robot.
  *
- *  This method put all actuators in the default state. The changes
- *   are applied immediately.
+ *  This method put all actuators in the default state, and disable the
+ *   usage of this module.
+ *
+ *  The default state is:
+ *   - Both motors stoped;
+ *   - Beacon sensor in the center position;
+ *   - All LEDs turned off.
+ *
+ *  *The changes are applied immediately*, without calling actuators_update().
  */
 void actuators_stop    ( void );
 
@@ -73,14 +93,15 @@ void actuators_stop    ( void );
 void actuators_setVel ( int left, int right );
 
 /**
- *  \brief Get the last velocitie values requested by calling actuators_setVel().
+ *  \brief Get the velocity values that will be applied next.
  *
- *  This function doesn't return the actual motors velocities. It does
- *   return the last values passed to actuators_setVel() as arguments.
+ *  This function doesn't return the current motors velocities. It does
+ *   return the values that will be applied to the robot if actuators_update()
+ *   was called at this moment.
  *
- *  If you are only interested in one of the values, simple give `NULL` to the
- *   other argument. Obviously both arguments can be `NULL` but I
- *   don't see the point.
+ *  Notice the values returned value can be different from the ones provided
+ *   with the actuators_setVel() function, because of constraints of the robot
+ *   (maximum velocity) or rounding of the values.
  *
  *  \param left Location where the value for the left motor should be stored
  *              or `NULL` if you're not interested in this value.
@@ -101,35 +122,47 @@ void actuators_getVel ( int* left, int* right );
  *  The value should be provided in the degrees. The change are only
  *   applied in the next call to actuators_update().
  *
- *  The maximum and minimun position depends upont the servo used. If the
- *   values provided exceeds the maximum position, the servo is set to the
- *   maximum.
+ *  The maximum and minimun position depends upont the servo used. The values
+ *   will be clipped to the limits when necessary.
  *
  *  The sensor is considered to be in the position 0 degres when it
- *   is pointing forward. The position increase clockwise, what means
+ *   is pointing forward. The position increase clockwise, what means that
  *   to rotate the servo clockwise a positive value should be provided
  *   and to rotate it anticlockwise a negative value should be provided.
  *
- *  To define the maximum degree the sensor could reach set the values in
- *   the configuration module (inc/robot/conf.h).
- *
  *  \param degree The next position to apply to the beacon sensor.
  */
-void actuators_setBeaconSens    ( int degree );
+void actuators_setBSensPos ( int degree );
 
 /**
  *  \brief Rotate the beacon sensor relatively to the current position.
  *
  *  The value should be provided in the degrees, and indicates how much
- *   the sensor should be rotated. The changes are only applied in the
- *   next call to actuators_update().
+ *   the sensor should be rotated from it's current position. The changes
+ *   are only applied in the next call to actuators_update().
  *
- *  Check actuators_setBeaconSens() for more details.
+ *  Check actuators_setBSensPos() for more details.
  *
  *  \param degree The value in degrees the sensor should
  *                be rotated from it's current position.
  */
-void actuators_rotateBeaconSens ( int degree );
+void actuators_rotateBSens ( int degree );
+
+/**
+ *  \brief Get the beacon sensor position that will be applied next.
+ *
+ *  This function doesn't return the current position. It does
+ *   return the values that will be applied to the robot if actuators_update()
+ *   was called at this moment.
+ *
+ *  Notice the values returned value can be different from the ones provided
+ *   with the actuators_setBSensPos() or actuators_rotateBSens()
+ *   functions, because of constraints of the robot (position limits) or rounding
+ *   of the values.
+ *
+ *  \param degree Location where the value of the position should be stored.
+ */
+void actuators_getBSensPos ( int* degree );
 
 
 /* ==========================================================================
@@ -141,7 +174,7 @@ void actuators_rotateBeaconSens ( int degree );
  *
  *  A true value will turn on the led, and a false value will turn it off.
  *
- *  If you try to set a led that doesn't exist then nothing happens, but
+ *  If you try to set a led that doesn't exist then nothing happens and
  *   false is returned.
  *
  *  \param ledN The led number to actuate on.
